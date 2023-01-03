744d21b3d1deb65d7c1c2c84cfa44e154017a3b0
jhanyne <amarachiisaac54@gmail.com>
2023-01-03 15:22:02 -0800

    desciption

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strspn - a function that the length of a prefix substring
+ *
+ * *Return: n bytes returned to the initial segment
+ */
+unsigned int _strspn(char *s, char *accept);
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = accept;
+		n--;
+	}
+	return (s)
+}
